9336743354b459b017f671ad1f1645abd1dcc1f2
Cory Prowse <cory@prowse.com>
2014-09-28 11:49:37 +0100

    Parsing does not work with ZonedDateTime due to bug in JDK

--- test/prowse/http/HttpHelpers.scala
+++ test/prowse/http/HttpHelpers.scala
@@ -1,20 +1,23 @@
 package prowse.http
 
 import java.time.format.DateTimeFormatter
-import java.time.{ZoneId, ZonedDateTime}
+import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
 import java.util.Locale
 
 import scala.util.Try
 
 trait HttpHelpers {
 
+  private val gmtZoneId: ZoneId = ZoneId.of("GMT")
+
   private val httpDateFormatter: DateTimeFormatter =
     DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.ENGLISH)
-      .withZone(ZoneId.of("GMT"))
+      .withZone(gmtZoneId)
 
   def isValidHttpDate(s: String) = Try(httpDateFormatter.parse(s)).isSuccess
 
-  def parseHttpDateString(s: String): Try[ZonedDateTime] = Try(ZonedDateTime.parse(s, httpDateFormatter))
+  // TODO: Remove atZone and switch to ZonedDateTime.parse method when fixed on Linux
+  def parseHttpDateString(s: String): Try[ZonedDateTime] = Try(LocalDateTime.parse(s, httpDateFormatter).atZone(gmtZoneId))
 
   def printHttpDateString(d: ZonedDateTime): String = httpDateFormatter.format(d)
 
